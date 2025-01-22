7d8c2ee718a5dc66b9e837145cb8c340d698ab93
YVismeh <Yvisme41148@gmail.com>
2025-01-22 14:30:46 -0600

    update

--- root/views.py
+++ root/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'root/index.html')
+
+
+
+# from django.http import HttpResponse
+# def home(request):
+#     return HttpResponse("<p>Hello, world. You're at the root index.</p>")
