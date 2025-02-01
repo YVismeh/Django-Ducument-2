4a37edfd5390b7610fcd33047f163fabfb2eba13
YVismeh <Yvisme41148@gmail.com>
2025-02-01 14:02:40 -0600

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,14 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 from captcha.fields import CaptchaField
 
-# class Captcha(forms.Form):
-#     captcha = CaptchaField()
-
+class Captcha(forms.Form):
+    captcha = CaptchaField()
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=120)
     password = forms.CharField(max_length=120, widget=forms.PasswordInput)
-    #captcha = CaptchaField()
+    captcha = CaptchaField()
 
 class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+class ChangePassForm(forms.Form):
+    password1 = forms.CharField(max_length=15)
+    password2 = forms.CharField(max_length=15)
+
+class ResetPassForm(forms.Form):
     email = forms.EmailField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm, SignUpForm#, Captcha
+from .forms import LoginForm, SignUpForm, Captcha, ChangePassForm, ResetPassForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth import password_validation
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
 
@@ -21,9 +23,15 @@ def login_user(request):
             else:
                 messages.add_message(request, messages.ERROR, "invalid data")
                 return redirect(request.path_info)
+        else:
+                messages.add_message(request, messages.ERROR, "invalid captcha")
+                return redirect(request.path_info)
      
     else:
-        return render(request, 'registration/login.html')
+        context = {
+            "form" : Captcha()
+        }
+        return render(request, 'registration/login.html', context=context)
 
 @login_required
 def logout_user(request):
@@ -44,15 +52,58 @@ def signup_user(request):
             messages.add_message(request, messages.ERROR, "invalid data")
             return redirect(request.path_info)
     else:
+        
         return render(request, 'registration/signup.html')
 
 
+@login_required
 def change_password(request):
-    return render(request, 'registration/change-password.html')
+    user = request.user
+    if request.method == "POST":
+        form = ChangePassForm(request.POST)
+        if form.is_valid():
+            pass1 = form.cleaned_data["password1"]
+            pass2 = form.cleaned_data["password2"]
+            if (pass1==pass2) and not (user.check_password(pass1)):
+                try:
+                    password_validation.validate_password(pass1)
+                except:
+                    messages.add_message(request, messages.ERROR, "Invalid password")
+                    return redirect(request.path_info) 
+                else:
+                    user.set_password(pass1)
+                    user.save()
+                    login(request, user)
+                    messages.add_message(request, messages.SUCCESS, "successfully changed password")        
+                    return redirect(request.path_info)
+            else:
+                messages.add_message(request, messages.ERROR, "pass1 and pass2 are'n similar or different with old one")  
+                return redirect(request.path_info)
+        else:
+            messages.add_message(request, messages.ERROR, "Invalid data")  
+            return redirect(request.path_info)
 
+    else:
+        return render(request, 'registration/change-password.html')
 
 def reset_password(request):
-    pass
+    if request.method == "POST":
+        form = ResetPassForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            try:
+                user = User.objects.get(email=email)
+            except:
+                messages.add_message(request, messages.ERROR, "email not found")  
+                return redirect(request.path_info)
+            else:
+                pass
+            token, create = Token.objects.get_or_create(user=user)
+            if not create:
+                Token.objects.get(user=user).delete()
+                token = Token.objects.create(user=user)
+    else:
+        return render(request, 'registration/reset-password.html')
 
 
 def reset_password_done(request):
